d6ca81a4c54bc055f043c6fb4393f213d3ad6c48
Sony Dwi Kurniawan <hitosony@gmail.com>
2018-08-15 11:26:59 +0700

    First Module

---
+++ todo_app/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Todo Application',
+    'description': 'Extend the To-Do app to multiuser.',
+    'author': 'Sony Dwi Kurniawan',
+    'data': [
+         'security/ir.model.access.csv',
+         'security/todo_access_rules.xml',
+         'views/todo_menu.xml',
+         'views/todo_view.xml'],
+    'application': True
+}
